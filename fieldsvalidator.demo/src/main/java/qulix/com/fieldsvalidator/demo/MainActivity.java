2f872d3240758ddc9aba59f31e2fd732c81cd7fe
Yablonsky, Andrey <ayablonsky@qulix.com>
2019-04-12 12:39:24 +0300

    Init empty project

---
+++ fieldsvalidator.demo/src/main/java/qulix/com/fieldsvalidator/demo/MainActivity.java
@@ -0,0 +1,22 @@
+package qulix.com.fieldsvalidator.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText field;
+    private Button validate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        field = findViewById(R.id.field);
+        validate = findViewById(R.id.validate);
+
+    }
+}
